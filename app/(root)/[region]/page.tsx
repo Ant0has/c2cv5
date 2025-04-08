de81f3b66931e4d20de42e34335f4f92dbb8a451
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-04-08 15:46:56 +0300

    added  changes for generateMetadata

--- app/(root)/[region]/page.tsx
+++ app/(root)/[region]/page.tsx
@@ -15,15 +15,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   if (!data) {
     return {};
   }
-
+  
   const siteName = "City2City";
   const page = `https://city2city.ru/${params.region}.html`;
-  const title =
-    data?.seo_title ||
-    `Такси ${data?.title} ${page} межгород ЦЕНА за Трансфер! Заказать междугороднее такси ${siteName}`;
-  const description =
-    data?.seo_description ||
-    `⭐️⭐️⭐️⭐️⭐️ Заказать междугороднее такси City2City по маршруту %%${data?.title} ${page}. Отличная цена, комфортные автомобили, проверенные водители. Заказ по телефонам: +7 (938) 156-87-578`;
+  const title = `Такси %%${data?.seoTitle}%% %%${page}%% межгород ЦЕНА за Трансфер! Заказать междугороднее такси %%${siteName}%%`;
+  const description = `⭐️⭐️⭐️⭐️⭐️ Заказать междугороднее такси City2City по маршруту %%${data?.seoTitle}%% %%${page}%%. Отличная цена, комфортные автомобили, проверенные водители. Заказ по телефонам: +7 (938) 156-87-578`;
 
   return {
     title,
