f93c802c8809f815ca0499a390b065b38e1e120e
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-07-04 18:45:37 +0300

    add changes to calculator

--- app/admin/calculator/Calculator.tsx
+++ app/admin/calculator/Calculator.tsx
@@ -1,124 +1,157 @@
-'use client'
+"use client";
 
 import SwapIcon from "@/public/icons/SwapIcon";
 import { planLabel } from "@/shared/components/Price/data";
 import Button from "@/shared/components/ui/Button/Button";
 import SearchInput from "@/shared/components/ui/SearchInput/SearchInput";
-import { COEFFICIENT_100, COEFFICIENT_100_150, COEFFICIENT_150_200, COEFFICIENT_200, prices, SPEED } from "@/shared/constants";
+import {
+  COEFFICIENT_100,
+  COEFFICIENT_100_150,
+  COEFFICIENT_150_200,
+  COEFFICIENT_200,
+  prices,
+  SPEED,
+} from "@/shared/constants";
 import { yandexMapsService } from "@/shared/services/yandex-maps.service";
 import { ButtonTypes, Prices } from "@/shared/types/enums";
-import { FullscreenControl, Map, RoutePanel, TrafficControl, ZoomControl } from "@pbe/react-yandex-maps";
+import {
+  FullscreenControl,
+  Map,
+  RoutePanel,
+  TrafficControl,
+  ZoomControl,
+} from "@pbe/react-yandex-maps";
 import clsx from "clsx";
-import { FC, useEffect, useRef, useState } from "react";
-import s from './Calculator.module.scss';
+import {
+  ChangeEvent,
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import s from "./Calculator.module.scss";
 
 interface IProps {
-  title?: unknown
+  title?: unknown;
 }
 
+const PLAN_COEFFICINT = "PLAN_COEFFICINT";
 
 const Calculator: FC<IProps> = () => {
-  const map = useRef<any>()
+  const map = useRef();
 
-  const [planCoefficient, setPlanCoefficient] = useState(prices)
-  const [selectedPlan, setSelectedPlan] = useState<Prices>(Prices.STANDARD)
+  const storagedData = localStorage.getItem(PLAN_COEFFICINT);
 
-  const [departurePoint, setDeparturePoint] = useState<string>()
-  const [departurePointData, setDeparturePointData] = useState<string[]>([])
-  const [arrivalPoint, setArrivalPoint] = useState<string>()
-  const [arrivalPointData, setArrivalPointData] = useState<string[]>([])
-  const [distance, setDistance] = useState<string>('-')
-  const [time, setTime] = useState<string>('-')
-  const [price, setPrice] = useState<number[]>()
+  const [planCoefficient, setPlanCoefficient] = useState(
+    storagedData ? JSON.parse(storagedData) : prices
+  );
+
+  const [departurePoint, setDeparturePoint] = useState<string>();
+  const [departurePointData, setDeparturePointData] = useState<string[]>([]);
+  const [arrivalPoint, setArrivalPoint] = useState<string>();
+  const [arrivalPointData, setArrivalPointData] = useState<string[]>([]);
+  const [distance, setDistance] = useState<string>("-");
+  const [time, setTime] = useState<string>("-");
+  const [price, setPrice] = useState<number[]>();
 
   const plans = [
     {
       key: Prices.STANDARD,
       label: planLabel[Prices.STANDARD],
-      coefficient: planCoefficient[Prices.STANDARD]
+      coefficient: planCoefficient[Prices.STANDARD],
     },
     {
       key: Prices.COMFORT,
       label: planLabel[Prices.COMFORT],
-      coefficient: planCoefficient[Prices.COMFORT]
+      coefficient: planCoefficient[Prices.COMFORT],
     },
     {
       key: Prices.COMFORT_PLUS,
       label: planLabel[Prices.COMFORT_PLUS],
-      coefficient: planCoefficient[Prices.COMFORT_PLUS]
+      coefficient: planCoefficient[Prices.COMFORT_PLUS],
     },
     {
       key: Prices.BUSINESS,
       label: planLabel[Prices.BUSINESS],
-      coefficient: planCoefficient[Prices.BUSINESS]
+      coefficient: planCoefficient[Prices.BUSINESS],
     },
     {
       key: Prices.MINIVAN,
       label: planLabel[Prices.MINIVAN],
-      coefficient: planCoefficient[Prices.MINIVAN]
+      coefficient: planCoefficient[Prices.MINIVAN],
     },
     {
       key: Prices.DELIVERY,
       label: planLabel[Prices.DELIVERY],
-      coefficient: planCoefficient[Prices.DELIVERY]
+      coefficient: planCoefficient[Prices.DELIVERY],
     },
-  ]
+  ];
 
   const handleClickSwapAddress = () => {
-    setDeparturePoint(arrivalPoint)
-    setArrivalPoint(departurePoint)
-    setDeparturePointData(arrivalPointData)
-    setArrivalPointData(departurePointData)
-  }
+    setDeparturePoint(arrivalPoint);
+    setArrivalPoint(departurePoint);
+    setDeparturePointData(arrivalPointData);
+    setArrivalPointData(departurePointData);
+  };
 
   const handleChangeDeparturePoint = (value: string) => {
-    setDeparturePoint(value)
-  }
+    setDeparturePoint(value);
+  };
 
   const handleSearchDeparturePoint = async (value: string) => {
-    const response = await yandexMapsService.getSuggestions(value)
+    const response = await yandexMapsService.getSuggestions(value);
     const uniqueData = [...new Set(response)];
-    setDeparturePointData(uniqueData)
-  }
-
-  //-----
+    setDeparturePointData(uniqueData);
+  };
 
   const handleChangeArrivalPoint = (value: string) => {
-    setArrivalPoint(value)
-  }
+    setArrivalPoint(value);
+  };
 
   const handleSearchArrivalPoint = async (value: string) => {
-    const response = await yandexMapsService.getSuggestions(value)
+    const response = await yandexMapsService.getSuggestions(value);
     const uniqueData = [...new Set(response)];
-    setArrivalPointData(uniqueData)
-  }
+    setArrivalPointData(uniqueData);
+  };
 
+  const handleChangePlanCoefficient =
+    (key: string) => (e: ChangeEvent<HTMLInputElement>) => {
+      const val = e.target.value;
 
-  const handleCalculate = async () => {
+      const changedData = { ...planCoefficient, [key]: val };
+
+      setPlanCoefficient(changedData);
+
+      console.log("changedData", changedData);
+
+      localStorage.setItem("PLAN_COEFFICINT", JSON.stringify(changedData));
+    };
+
+  const handleCalculate = useCallback(async () => {
     if (map?.current) {
       const control = await map.current.routePanel.getRouteAsync();
       const activeRoute = control.getActiveRoute();
       if (activeRoute) {
-        const distance = activeRoute.properties.get("distance")
-        const duration = activeRoute.properties.get("duration")
+        const distance = activeRoute.properties.get("distance");
         // console.log('duration', duration)
 
         // setDistance(distance?.text)
         // setTime(duration?.text)
         if (distance?.value) {
-          const distanceValue = Math.ceil((distance?.value / 1000) / 10) * 10;
+          const distanceValue = Math.ceil(distance?.value / 1000 / 10) * 10;
 
           const convertHoursToRoundedTime = (hours: number): string => {
-            let totalMinutes = Math.ceil(hours * 60 / 30) * 30;
+            const totalMinutes = Math.ceil((hours * 60) / 30) * 30;
 
-            let totalHours = Math.floor(totalMinutes / 60);
-            let weeks = Math.floor(totalHours / (24 * 7));
-            let remainingHoursAfterWeeks = totalHours % (24 * 7);
-            let days = Math.floor(remainingHoursAfterWeeks / 24);
-            let roundedHours = remainingHoursAfterWeeks % 24;
-            let roundedMinutes = totalMinutes % 60;
+            const totalHours = Math.floor(totalMinutes / 60);
+            const weeks = Math.floor(totalHours / (24 * 7));
+            const remainingHoursAfterWeeks = totalHours % (24 * 7);
+            const days = Math.floor(remainingHoursAfterWeeks / 24);
+            const roundedHours = remainingHoursAfterWeeks % 24;
+            const roundedMinutes = totalMinutes % 60;
 
-            let result = [];
+            const result = [];
             if (weeks > 0) result.push(`${weeks} нед`);
             if (days > 0) result.push(`${days} дн`);
             if (roundedHours > 0) result.push(`${roundedHours} ч`);
@@ -128,60 +161,74 @@ const Calculator: FC<IProps> = () => {
           };
 
           const getCoefficient = (distance: number) => {
-            if (distance < 100) return COEFFICIENT_100
-            if (distance >= 100 && distance < 150) return COEFFICIENT_100_150
-            if (distance >= 150 && distance < 200) return COEFFICIENT_150_200
-            return COEFFICIENT_200
-          }
+            if (distance < 100) return COEFFICIENT_100;
+            if (distance >= 100 && distance < 150) return COEFFICIENT_100_150;
+            if (distance >= 150 && distance < 200) return COEFFICIENT_150_200;
+            return COEFFICIENT_200;
+          };
 
           const getPrice = () => {
-            const result = []
+            const result = [];
             for (const key in planCoefficient) {
               if (Object.prototype.hasOwnProperty.call(planCoefficient, key)) {
                 const currentPrice = planCoefficient[key as Prices];
-                const initialPrice = distanceValue * currentPrice * getCoefficient(distanceValue)
-                result.push(Math.ceil(initialPrice / 500) * 500)
+                const initialPrice =
+                  distanceValue * currentPrice * getCoefficient(distanceValue);
+                result.push(Math.ceil(initialPrice / 500) * 500);
               }
             }
-            return result
-          }
+            return result;
+          };
 
-          const timeValue = convertHoursToRoundedTime(distanceValue / SPEED)
+          const timeValue = convertHoursToRoundedTime(distanceValue / SPEED);
 
-          setDistance(`${distanceValue} км`)
-          setTime(timeValue)
-          setPrice(getPrice())
+          setDistance(`${distanceValue} км`);
+          setTime(timeValue);
+          setPrice(getPrice());
         }
       }
     }
 
     if (!departurePoint || !arrivalPoint) return;
-  };
+  }, [arrivalPoint, departurePoint, planCoefficient, map]);
 
   useEffect(() => {
-    handleCalculate()
-  }, [planCoefficient])
+    handleCalculate();
+  }, [handleCalculate, planCoefficient]);
 
   return (
     <div className={clsx("container", s.wrapper)}>
       <div className={s.plans}>
-        {plans.map(plan => (
+        {plans.map((plan) => (
           <div key={plan.key} className={clsx(s.container)}>
             <div className={s.button}>{plan.label}</div>
-            <input className={s.input} type="number" value={plan.coefficient} onChange={e => setPlanCoefficient(prev => ({
-              ...prev,
-              [plan.key]: e.target.value
-            }))} />
+            <input
+              className={s.input}
+              type="number"
+              value={plan.coefficient}
+              onChange={handleChangePlanCoefficient(plan.key)}
+            />
           </div>
         ))}
+        <div
+          className={s.returnDefault}
+          onClick={() => {
+            setPlanCoefficient(prices);
+            localStorage.setItem(PLAN_COEFFICINT, JSON.stringify(prices));
+          }}
+        >
+          Вернуть значения по умолчаню
+        </div>
       </div>
 
       <div className={s.block}>
         <div className={s.selection}>
           <div className={s.part}>
-            <div className={clsx(s.label, 'font-16-normal')}>Точка отправления</div>
+            <div className={clsx(s.label, "font-16-normal")}>
+              Точка отправления
+            </div>
             <SearchInput
-              className='departure-select address-select'
+              className="departure-select address-select"
               value={departurePoint}
               placeholder="Санкт-Петербург"
               data={departurePointData}
@@ -191,18 +238,17 @@ const Calculator: FC<IProps> = () => {
           </div>
 
           <div className={s.swapButtonWrapper}>
-            <div
-              onClick={handleClickSwapAddress}
-              className={s.swapButton}>
+            <div onClick={handleClickSwapAddress} className={s.swapButton}>
               <SwapIcon />
             </div>
           </div>
 
-
           <div className={s.part}>
-            <div className={clsx(s.label, 'font-16-normal')}>Точка прибытия</div>
+            <div className={clsx(s.label, "font-16-normal")}>
+              Точка прибытия
+            </div>
             <SearchInput
-              className='arrival-select address-select'
+              className="arrival-select address-select"
               value={arrivalPoint}
               placeholder="Пенза"
               data={arrivalPointData}
@@ -222,18 +268,24 @@ const Calculator: FC<IProps> = () => {
         </div>
 
         <div className={s.info}>
-          <span>Стоимость:	</span>
+          <span>Стоимость: </span>
           <div className={s.priceGrid}>
-            {price ? price?.map((el, id) => <div className={s.priceElement}>
-              <div className={s.pricePlan}>{plans[id].label}</div>
-              <div className={s.priceValue}>{el}р</div>
-              <div className={s.pricePlan}>{plans[id].coefficient}р за км</div>
-            </div>) : <span className="font-18-semibold">-</span>}
+            {price ? (
+              price?.map((el, id) => (
+                <div className={s.priceElement} key={id}>
+                  <div className={s.pricePlan}>{plans[id].label}</div>
+                  <div className={s.priceValue}>{el}р</div>
+                  <div className={s.pricePlan}>
+                    {plans[id].coefficient}р за км
+                  </div>
+                </div>
+              ))
+            ) : (
+              <span className="font-18-semibold">-</span>
+            )}
           </div>
-
         </div>
       </div>
-
 
       <Button
         disabled={!departurePoint || !arrivalPoint}
@@ -245,38 +297,40 @@ const Calculator: FC<IProps> = () => {
       <div></div>
 
       <div className={s.map}>
-        <Map width={'100%'} height={500} defaultState={{
-          center: [55.751574, 37.573856],
-          zoom: 9,
-          controls: [],
-        }}>
+        <Map
+          width={"100%"}
+          height={500}
+          defaultState={{
+            center: [55.751574, 37.573856],
+            zoom: 9,
+            controls: [],
+          }}
+        >
           <ZoomControl />
           <FullscreenControl />
           <TrafficControl />
           {/* <TypeSelector /> */}
           <RoutePanel
-
             options={{ float: "right", visible: false }}
             instanceRef={(ref: any) => {
-              map.current = ref
+              debugger;
+              map.current = ref;
               if (ref) {
                 ref.routePanel.state.set({
-                  from: departurePoint || '',
-                  to: arrivalPoint || ''
+                  from: departurePoint || "",
+                  to: arrivalPoint || "",
                 });
 
                 ref.routePanel.options.set({
                   autoSelect: true,
-
                 });
               }
             }}
           />
         </Map>
       </div>
-
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default Calculator;
--- shared/components/ui/SearchInput/SearchInput.tsx
+++ shared/components/ui/SearchInput/SearchInput.tsx
@@ -1,43 +1,134 @@
-import { Select } from 'antd';
-import { FC } from 'react';
+import { Input, Select } from "antd";
+import { FC, useEffect, useState } from "react";
 
 interface IProps {
   placeholder: string;
-  value?: string
-  data?: string[]
-  className?: string
-  handleSearch: (value: string) => void,
-  handleChange: (value: string) => void
+  value?: string;
+  data?: string[];
+  className?: string;
+  handleSearch: (value: string) => void;
+  handleChange: (value: string) => void;
 }
 
 const SearchInput: FC<IProps> = (props) => {
-  const {
-    className,
-    placeholder,
-    value,
-    data,
-    handleSearch,
-    handleChange
-  } = props
+
+  return (
+    <CustomSelect
+    {...props}
+    // className={className}
+    // showSearch
+    // value={value}
+    // placeholder={placeholder}
+    // defaultActiveFirstOption={false}
+    // suffixIcon={null}
+    // filterOption={false}
+    // onSearch={handleSearch}
+    // onChange={handleChange}
+    // notFoundContent={null}
+    // options={(data || []).map((d) => ({
+    //   value: d,
+    //   label: d,
+    // }))}
+    />
+  );
+};
+
+export default SearchInput;
+
+const CustomSelect: FC<IProps> = (props) => {
+  const { className, placeholder, value, data, handleSearch, handleChange } =
+    props;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSelectChange = (val:string) => {
+    setInputValue(val);
+    handleChange(val)
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    setIsEditing(false);
+  };
+
+  useEffect(() => {
+    setInputValue(value ?? "");
+  }, [value]);
+
+  // className={className}
+  // showSearch
+  // value={value}
+  // placeholder={placeholder}
+  // defaultActiveFirstOption={false}
+  // suffixIcon={null}
+  // filterOption={false}
+  // onSearch={handleSearch}
+  // onChange={handleChange}
+  // notFoundContent={null}
+  // options={(data || []).map((d) => ({
+  //   value: d,
+  //   label: d,
+  // }))}
 
   return (
     <Select
       className={className}
       showSearch
       value={value}
+      onChange={handleSelectChange}
       placeholder={placeholder}
       defaultActiveFirstOption={false}
       suffixIcon={null}
       filterOption={false}
       onSearch={handleSearch}
-      onChange={handleChange}
+      // onChange={handleChange}
       notFoundContent={null}
-      options={(data || []).map((d) => ({
-        value: d,
-        label: d,
-      }))}
-    />
+      dropdownRender={(menu) => (
+        <div>
+          {menu}
+          {isEditing ? (
+            <Input
+              value={inputValue}
+              onChange={handleInputChange}
+              onBlur={handleInputBlur}
+              autoFocus
+              style={{ margin: "8px", width: "90%" }}
+            />
+          ) : (
+            <div
+              style={{
+                padding: "8px",
+                cursor: "pointer",
+                textAlign: "center",
+                background: "#fafafa",
+              }}
+              onClick={handleEditClick}
+            >
+              Редактировать
+            </div>
+          )}
+        </div>
+      )}
+      style={{ width: "100%" }}
+          options={(data || []).map((d) => ({
+      value: d,
+      label: d,
+    }))}
+    >
+      {/* <Option value="Длинное название маршрута 1">
+        Длинное название маршрута 1
+      </Option>
+      <Option value="Длинное название маршрута 2">
+        Длинное название маршрута 2
+      </Option> */}
+    </Select>
   );
 };
-
-export default SearchInput;
