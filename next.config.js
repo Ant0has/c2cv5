1e2b8d5af836538ea8b9a95d677575aec4a6cd20
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-09-17 20:54:31 +0300

    fix: bring back settings in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,36 +7,36 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['heavy-package'],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       // index.html → корень
-  //       source: '/index.html',
-  //       destination: '/',
-  //     },
-  //     {
-  //       // любые другие .html → рендерим без него
-  //       source: '/:path*.html',
-  //       destination: '/:path*',
-  //     },
-  //   ]
-  // },
-  // async redirects() {
-  //   return [
-  //     {
-  //       // с / редиректим на /index.html
-  //       source: '/',
-  //       destination: '/index.html',
-  //       permanent: true,
-  //     },
-  //     {
-  //       // редиректим только если нет .html на конце
-  //       source: '/:path((?!.*\\.html$).*)',
-  //       destination: '/:path.html',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        // index.html → корень
+        source: '/index.html',
+        destination: '/',
+      },
+      {
+        // любые другие .html → рендерим без него
+        source: '/:path*.html',
+        destination: '/:path*',
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        // с / редиректим на /index.html
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
+      },
+      {
+        // редиректим только если нет .html на конце
+        source: '/:path((?!.*\\.html$).*)',
+        destination: '/:path.html',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
