85e8097984fd0941b5a98d0119ead155acfd5546
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-10-08 12:55:47 +0300

    fix: update sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,8 +1,13 @@
 module.exports = {
-    siteUrl: 'https://city2city.ru',
-    generateRobotsTxt: true, // создаст файл robots.txt автоматически
-    sitemapSize: 5000,
-    changefreq: 'daily',
-    priority: 0.7,
-    exclude: ['/admin/**'], // исключение не индексируемых страниц
+  siteUrl: 'https://city2city.ru',
+  generateRobotsTxt: true, // создаст файл robots.txt автоматически
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: [
+    '/admin/**',
+    '/admin/calculator',
+    '/admin/other-hidden-page', 
+    '/calculator',
+     ]
   }
