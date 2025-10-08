7f63d83345ed78b0c5862dad301f5af5096ceb11
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-10-08 11:00:39 +0300

    fix: hide calculator page from indexation

--- app/admin/calculator/page.tsx
+++ app/admin/calculator/page.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from 'next'
 import Calculator from './Calculator'
 
-
-
 export const metadata: Metadata = {
-  title: 'Каклькулятор'
+  title: 'Каклькулятор',
+  description: '',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
 }
 
 export default async function OfertaPage() {
