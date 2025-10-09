54cdb86bf2a2b5dea106dcffa9a36b4c539b1cfd
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-10-09 14:08:57 +0300

    fix: add styles for Header

--- widgets/Header/ui/HeaderPhones.tsx
+++ widgets/Header/ui/HeaderPhones.tsx
@@ -1,10 +1,11 @@
 import PhoneIcon from '@/public/icons/PhoneIcon';
 import { PHONE_NUMBER_FIRST } from '@/shared/constants';
 import Link from 'next/link';
+import s from '../Header.module.scss';
 
 const HeaderPhones = () => {
     return (
-        <div>
+        <div className={s.phones}>
             <Link target='_blank' href={`tel:+${PHONE_NUMBER_FIRST}`}>
                 <PhoneIcon />
             </Link>
