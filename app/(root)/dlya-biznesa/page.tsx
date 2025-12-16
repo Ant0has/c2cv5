173a8879f68556667d16e83b7df9ef2a22968e6d
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-12-16 11:11:33 +0000

    feat: add new page for business & add chat

---
+++ app/(root)/dlya-biznesa/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next'
+import DlyaBiznesa from './DlyaBiznesa'
+
+export const metadata: Metadata = {
+  title: 'City2City для бизнеса — корпоративные трансферы и перевозки',
+  description: 'Корпоративный трансфер сотрудников. 8 лет работы, более 9000 трансферов, 79 регионов присутствия. Оплата по счету, работа с юрлицами, ЭДО.Диадок.',
+  keywords: 'корпоративный трансфер, такси для бизнеса, перевозка сотрудников, трансфер для юрлиц, междугородний трансфер для компаний',
+}
+
+export default async function DlyaBiznesaPage() {
+  return <DlyaBiznesa />
+}
