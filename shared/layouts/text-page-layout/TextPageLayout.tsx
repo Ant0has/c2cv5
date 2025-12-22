6eabf3183a4936ecd98ff3590482771bdd8ece28
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-12-22 20:54:30 +0300

    fix: update PrivacyPolicyPage

---
+++ shared/layouts/text-page-layout/TextPageLayout.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+import s from './TextPageLayout.module.scss';
+import Breadcrumbs from '@/shared/components/Breadcrumbs/Breadcrumbs';
+
+const TextPageLayout = ({ children,title }: { children: React.ReactNode,title: string }) => {
+    return (
+        <div className={'container'}>
+            <div className={s.inner}>
+                <Breadcrumbs />
+                <h1 className={clsx('title',s.title)}>{title}</h1>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default TextPageLayout;
