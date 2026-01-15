9df5660baa0368fd3b7b2fd5febb4ff625480f4d
TrocyukS96 <trotzuk.stanislav@gmail.com>
2026-01-15 15:29:38 +0300

    fix: add changes to middleware

--- middleware.ts
+++ middleware.ts
@@ -17,13 +17,13 @@ export function middleware(request: NextRequest) {
   }
 
   // 2. Вторая задача: проверка страниц маршрутов на наличие в whitelist
-  if (pathname.endsWith('.html') && pathname !== '/') {
-    const slug = pathname.slice(1)
+  // if (pathname.endsWith('.html') && pathname !== '/') {
+  //   const slug = pathname.slice(1)
     
-    if (!whitelist.includes(slug)) {
-      return NextResponse.rewrite(new URL('/404', request.url), { status: 404 })
-    }
-  }
+  //   if (!whitelist.includes(slug)) {
+  //     return NextResponse.rewrite(new URL('/404', request.url), { status: 404 })
+  //   }
+  // }
 
   return NextResponse.next()
 }
